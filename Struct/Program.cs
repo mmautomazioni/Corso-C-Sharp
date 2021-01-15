ddbdac1876403b0fa883bde84a1b94f640b4e6eb
malbasini <malbasini@gmail.com>
2021-01-16 00:11:00 +0100

    Introdotto esercitazione

--- Struct/Program.cs
+++ Struct/Program.cs
@@ -13,7 +13,27 @@ namespace Struct
                               //A DIFFERENZA DELLE CLASSI.
             persona.nome="Marco";
             persona.cognome="Rossi";
-            
+            //ESERCITAZIONE CON LE STRUCT
+            MyClass mc = new MyClass();
+            MyStruct ms = new MyStruct();
+            mc.eta=40;
+            mc.nome="Mario";
+            ms.eta=60;
+            ms.nome="Fabio";
+            Console.WriteLine($"Prima della chiamata mc.eta=: {mc.eta} ms.eta=: {ms.eta}");
+            MyMethod(mc,ms,"Pippo",20);
+            Console.WriteLine($"Dopo la chiamata mc.eta=: {mc.eta} ms.eta=: {ms.eta}");
+            /*--LE STRUCT ESSENDO DEI VALUE TYPES PASSANO AL METODO UNA COPIA DEI LORO ATTRIBUTI
+            AL RITORNO DAL METODO I VALORI NON SONO MODIFICATI. vICEVERSA LE CLASSI ESSENDO
+            DEI REFERENCE TYPES PASSANO UNA COPIA DEL RIFERIMENTO CHE PUNTA ALLA MEDESIMA STRUTTURA
+            CREATA NELL'HEAP, AL RITORNO L'ISTANZA mc SARA' MODIFICATA.*/
+        }
+        static void MyMethod(MyClass mc, MyStruct ms, string myString, int myInt )
+        {
+            mc.nome=myString;
+            mc.eta=myInt;
+            ms.nome=myString;
+            ms.eta=myInt;
         }
     }
     public struct Azienda
@@ -34,4 +54,15 @@ namespace Struct
         public string nome;
         public string cognome;
     }
+    public class MyClass
+    {
+        public string nome;
+        public int eta;
+    }
+    public struct MyStruct
+    {
+        public string nome;
+        public int eta;
+    }
+
 }
