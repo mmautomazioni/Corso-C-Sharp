a2db99a147aa8663aff483352de642ea1e3bfe2a
malbasini <malbasini@gmail.com>
2021-01-18 01:07:44 +0100

    Enumerazioni

---
+++ Enumerazioni/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enumerazioni
+{
+    enum GiorniSettimana:byte{Lunedi=10,martedi=20,mercoledi=30,giovedi,venerdi,sabato,domenica};
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           GiorniSettimana gs = GiorniSettimana.domenica;
+           if(gs==GiorniSettimana.domenica)
+              Console.WriteLine("Domenica");
+           int giorno = (int)GiorniSettimana.domenica;
+           /*--POSSIAMO ACCEDERE ALL'INTERO CHE STA DIETRO ALL'ENUMERAZIONE CON UN SEMPLICE CAST*/
+           Console.WriteLine(giorno);   
+        }
+    }
+}
