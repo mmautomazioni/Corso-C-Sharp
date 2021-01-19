1883973c6fd50ea09d841e1d10628aa911c6fdb7
malbasini <malbasini@gmail.com>
2021-01-19 05:22:28 +0100

    Ereditarieta

--- Ereditarieta/Program.cs
+++ Ereditarieta/Program.cs
@@ -2,11 +2,34 @@
 
 namespace Ereditarieta
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Mammifero m = new Mammifero("Pippo",20);   
         }
     }
+    public class Animale
+    {
+        private int myInt;
+        public string MyProperty {get;set;}
+        public Animale(int myInt)
+        {
+             this.myInt=myInt;
+        }
+        public void Respira()
+        {
+            Console.WriteLine("Respira");
+        }
+    }
+    public class Mammifero : Animale
+    {
+        private string myString;
+        public Mammifero(string myString,int myInt):base(myInt)
+        {
+              this.myString=myString;
+        }
+        
+    }
 }
+
