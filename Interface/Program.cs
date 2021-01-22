6137c1201d255d138bf2debac059ddb6020d17a9
malbasini <malbasini@gmail.com>
2021-01-22 04:41:26 +0100

    Interface

---
+++ Interface/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          MyClassA a = new MyClassA();
+          IMyInterface i = a as IMyInterface;
+          ((MyClassA)i).MyMethod2();//PER INVOCARE I METODI APPARTENENTI SOLO
+                                    //A MyClassA OCCORRE FARE IL CASTING.
+          MyClassB.MyMethod(i);
+        }
+        public interface IMyInterface
+        {
+            void MyMethod();
+        }
+        
+        public class MyClassA : IMyInterface
+        {
+            public void MyMethod()
+            {
+                Console.WriteLine("MyMethod() di MyClassA");
+            }
+            public void MyMethod2()
+            {
+                Console.WriteLine("MyMethod2() di MyClassA");
+            }
+        }
+        public class MyClassB
+        {
+            public static void MyMethod(IMyInterface myInterface)
+            {
+                myInterface.MyMethod();
+            }
+        }
+       
+    }
+}
+
