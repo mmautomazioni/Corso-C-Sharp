4702b2d92dadf26a8da98d53993b0f69ec13b1be
malbasini <malbasini@gmail.com>
2021-01-24 05:19:43 +0100

    Eccezioni in C#

---
+++ Eccezioni/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Eccezioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            Console.Write("Inserisci il Dividendo: ");
+            string dividendo = Console.ReadLine();
+            Console.Write("Inserisci il Divisore: ");
+            string divisore = Console.ReadLine();
+            try
+            {
+                int num1 = Convert.ToInt32(dividendo);
+                int num2 = Convert.ToInt32(divisore);
+                result = Divisione(num1,num2);
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine($"Hai inserito lo zero come divisore, eccezione generata {e.Message}");
+            }
+            catch(InvalidCastException e)
+            {
+                Console.WriteLine($"Hai inserito valori non validi eccezione generata {e.Message}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Si è verificata questa eccezione: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine(result);
+            }
+           
+        }
+        static int Divisione(int a, int b)
+        {
+            try
+            {
+               return a/b; 
+            }
+            catch(Exception ex)
+            {
+                throw new DivideByZeroException("Divisione per zero",ex);
+            }
+            
+        }
+    }
+}
