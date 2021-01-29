91a3be700ef57653ec14807cb3aa89cdc9b62914
malbasini <malbasini@gmail.com>
2021-01-29 09:19:13 +0100

    Commit

--- EsercitazioneEreditarieta/Program.cs
+++ EsercitazioneEreditarieta/Program.cs
@@ -1,117 +1,47 @@
 ﻿using System;
 
-namespace EsercitazioneEreditarieta
+namespace Interface
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Utente utente1 = new Utente("1234560","Mario","Rossi","4356789");
-            utente1.Saldo = 13000m;
-            Utente utente2 = new Utente("12345678","Mario","Verdi","435679879");
-            utente2.Saldo = 30000m;
-            utente1.SaldoCorrente();
-            utente2.SaldoCorrente();
-            Banca b1 = new Banca("1234567890","UBI Banca",utente1);
-            Banca b2 = new Banca("1234576543","UBI Banca",utente2);
-            b1.Preleva(14000m);
-            b2.Preleva(10000m);
-            utente2.SaldoCorrente();
+          MyClassA a = new MyClassA();
+          a.MyMethod();
+          a.MyMethod1();
+          a.MyProperty2 = "Corso C#.";
+          Console.WriteLine(a.MyProperty2);
         }
     }
-    internal class Persona
+    public interface IMyInterface1
     {
-             public string Nome {get;set;}
-             public string Cognome {get;set;}
-
-             public string Denominazione
-             {
-                 
-                 get{
-                     return "Utente " + this.Nome + " " + this.Cognome;
-                 }
-                
-             }
+        void MyMethod1();
+        string MyProperty1 {get;set;}
     }
-    internal class Utente : Persona
+    public interface IMyInterface2
     {
-             private decimal saldo;
-             public string Id {get;set;}
-            
-             public decimal Saldo
-             {
-                 get{
-                     return this.saldo;
-                 }
-                 set{
-                     if(value<0)
-                        Console.WriteLine($"Saldo negativo. Impossibile impostare il saldo. Valore passato: {value}");
-                     else
-                        this.saldo=value;
-                 }
-            }   
-
-             public string NumeroConto {get;set;}
-
-             public Utente(string id,string nome, string cognome, string numeroConto)
-             {
-                 this.Id=id;
-                 this.Nome=nome;
-                 this.Cognome=cognome;
-                 this.NumeroConto=numeroConto;
-             }
-             internal void SaldoCorrente()
-             {
-                 Console.WriteLine($"Utente {this.Nome + " " +this.Cognome} saldo corrente: {this.Saldo}");
-             }
+        void MyMethod2();
+        string MyProperty2 {get;set;}
     }
-    internal class Banca
+    public class MyClassA : MyClassB, IMyInterface1, IMyInterface2
     {
-            public string Id {get;set;}
-            public string Denominazione {get;set;}
-            public string Abi {get;set;}
-            public string Cab {get;set;}
-            public Utente Utente {get;set;}
-            public Banca(string id, string denominazione,Utente utente,string abi, string cab)
-            {
-                this.Id=id;
-                this.Denominazione=denominazione;
-                this.Utente=utente;
-                this.Abi=abi;
-                this.Cab=cab;
-            }
-            public Banca(string id,string denominazione,Utente utente):this(id,denominazione,utente,"06098","14400"){}
+        public void MyMethod1()
+        {
+            Console.WriteLine("Sto implmentando MyMethod1() dell 'interfaccia IMyInteface1");
+        }
+        public string MyProperty1{get;set;}
 
-            internal void Preleva(decimal importo)
-            {
-                decimal saldo = Utente.Saldo;
-                if(importo>saldo)
-                {
-                    Console.WriteLine($"Scoperto: Operazione di prelievo non ammessa, importo richiesto {importo}");
-                    return;
-                    
-                }
-                if(importo<0)
-                {
-                    Console.WriteLine($"Importo prelevato non valido: {importo}");
-                    return;
-                }
-                Utente.Saldo = Utente.Saldo - importo;
-                return;
-
-            }
-            internal void Accredito(decimal importo)
-            {
-                if(importo<0)
-                {
-                    Console.WriteLine($"Importo accreditato non valido: {importo}");
-                    return;
-                }
-                Utente.Saldo = Utente.Saldo +importo;
-                return;
-            }
-            
+        public void MyMethod2()
+        {
+            Console.WriteLine("Sto implmentando MyMethod2() dell 'interfaccia IMyInteface2.");
+        }
+        public string MyProperty2{get;set;}
+    } 
+    public class MyClassB
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("Sto eseguendo MyMethod() di MyClassB.");
+        }
     }
 }
-
-
--- Eventi/Program.cs
+++ Eventi/Program.cs
@@ -25,100 +25,100 @@ namespace Eventi
     }
     internal class Utente
     {
-             public event Action<decimal> NewSaldo;//DEFINIZIONE DI UN EVENTO
-             public void OnNewSaldo(decimal saldo)
-             {
-                 if(NewSaldo!=null) //SE NON CI SONO ASCOLTATORI REGISTRATI NewSaldo VALE NULL.
-                     NewSaldo(saldo);//EMETTO L'EVENTO A TUTTI I SOTTOSCRITTORI.
-             }
-             private decimal saldo;
-             public string Id {get;set;}
-             public string Nome {get;set;}
-             public string Cognome {get;set;}
-             public decimal Saldo
-             {
-                 get{
-                     return this.saldo;
-                 }
-                 set{
-                     if(value<0)
-                        Console.WriteLine($"Saldo negativo. Impossibile impostare il saldo. Valore passato: {value}");
-                     else
-                     {
-                        this.saldo=value;
-                        OnNewSaldo(value);//SE VIENE CAMBIATO IL SALDO EMETTO L'EVENTO SE CI SONO
+        public event Action<decimal> NewSaldo;//DEFINIZIONE DI UN EVENTO
+        public void OnNewSaldo(decimal saldo)
+        {
+            if(NewSaldo!=null) //SE NON CI SONO ASCOLTATORI REGISTRATI NewSaldo VALE NULL.
+                NewSaldo(saldo);//EMETTO L'EVENTO A TUTTI I SOTTOSCRITTORI.
+        }
+        private decimal saldo;
+        public string Id {get;set;}
+        public string Nome {get;set;}
+        public string Cognome {get;set;}
+        public decimal Saldo
+        {
+            get{
+                    return this.saldo;
+                }
+            set{
+                if(value<0)
+                    Console.WriteLine($"Saldo negativo. Impossibile impostare il saldo. Valore passato: {value}");
+                else
+                {
+                    this.saldo=value;
+                    OnNewSaldo(value);//SE VIENE CAMBIATO IL SALDO EMETTO L'EVENTO SE CI SONO
                                           //DEI LISTNER ATTIVI.
-                     }
-                 }
-             }   
+                }
+            }
+        }   
 
-             public string NumeroConto {get;set;}
+        public string NumeroConto {get;set;}
 
-             public Utente(string id,string nome, string cognome, string numeroConto)
-             {
-                 this.Id=id;
-                 this.Nome=nome;
-                 this.Cognome=cognome;
-                 this.NumeroConto=numeroConto;
-             }
-             public string Denominazione
-             {
+        public Utente(string id,string nome, string cognome, string numeroConto)
+        {
+            this.Id=id;
+            this.Nome=nome;
+            this.Cognome=cognome;
+            this.NumeroConto=numeroConto;
+        }
+        public string Denominazione
+        {
                  
-                 get{
-                     return "Utente " + this.Nome + " " + this.Cognome + " numero di conto: "+this.NumeroConto;
-                 }
+            get{
+                return "Utente " + this.Nome + " " + this.Cognome + " numero di conto: "+this.NumeroConto;
+            }
                 
-             }
-             internal void SaldoCorrente()
-             {
-                 Console.WriteLine($"Utente {this.Nome + " " +this.Cognome} saldo corrente: {this.Saldo}");
-             }
+        }
+        internal void SaldoCorrente()
+        {
+            Console.WriteLine($"Utente {this.Nome + " " +this.Cognome} saldo corrente: {this.Saldo}");
+        }
     }
     internal class Banca
     {
-            public string Id {get;set;}
-            public string Denominazione {get;set;}
-            public string Abi {get;set;}
-            public string Cab {get;set;}
-            public Utente Utente {get;set;}
-            public Banca(string id, string denominazione,Utente utente,string abi, string cab)
-            {
-                this.Id=id;
-                this.Denominazione=denominazione;
-                this.Utente=utente;
-                this.Abi=abi;
-                this.Cab=cab;
-            }
-            public Banca(string id,string denominazione,Utente utente):this(id,denominazione,utente,"06098","14400"){}
+        public string Id {get;set;}
+        public string Denominazione {get;set;}
+        public string Abi {get;set;}
+        public string Cab {get;set;}
+        public Utente Utente {get;set;}
+        public Banca(string id, string denominazione,Utente utente,string abi, string cab)
+        {
+            this.Id=id;
+            this.Denominazione=denominazione;
+            this.Utente=utente;
+            this.Abi=abi;
+            this.Cab=cab;
+        }
+        public Banca(string id,string denominazione,Utente utente):this(id,denominazione,utente,"06098","14400"){}
 
-            internal void Preleva(decimal importo)
+        internal void Preleva(decimal importo)
+        {
+            decimal saldo = Utente.Saldo;
+            if(importo>saldo)
             {
-                decimal saldo = Utente.Saldo;
-                if(importo>saldo)
-                {
-                    Console.WriteLine($"Scoperto: Operazione di prelievo non ammessa, importo richiesto {importo}");
-                    return;
+                Console.WriteLine($"Scoperto: Operazione di prelievo non ammessa, importo richiesto {importo}");
+                return;
                     
-                }
-                if(importo<0)
-                {
-                    Console.WriteLine($"Importo prelevato non valido: {importo}");
-                    return;
-                }
-                Utente.Saldo = Utente.Saldo - importo;
-                return;
-
             }
-            internal void Accredito(decimal importo)
+            if(importo<0)
             {
-                if(importo<0)
-                {
-                    Console.WriteLine($"Importo accreditato non valido: {importo}");
-                    return;
-                }
-                Utente.Saldo = Utente.Saldo +importo;
+                Console.WriteLine($"Importo prelevato non valido: {importo}");
                 return;
             }
+            Utente.Saldo = Utente.Saldo - importo;
+            return;
+
+        }
+        internal void Accredito(decimal importo)
+        {
+            if(importo<0)
+            {
+                Console.WriteLine($"Importo accreditato non valido: {importo}");
+                return;
+            }
+            Utente.Saldo = Utente.Saldo +importo;
+            return;
+        }
             
     }
 }
