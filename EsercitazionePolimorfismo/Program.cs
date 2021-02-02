8e24a80c3fe1afe9c79be19553e8ac0f59312605
malbasini <malbasini@gmail.com>
2021-02-02 03:44:17 +0100

    Commi classe program

--- EsercitazionePolimorfismo/Program.cs
+++ EsercitazionePolimorfismo/Program.cs
@@ -46,7 +46,7 @@ namespace EsercitazionePolimorfismo
                  }
                  set{
                      if(value<0)
-                        Console.WriteLine($"Saldo negativo. Impossibile impostare il saldo. Valore passato: {value}");
+                        Console.WriteLine($"Impossibile impostare il saldo. Valore passato: {value}");
                      else
                         this.saldo=value;
                  }
@@ -96,14 +96,15 @@ namespace EsercitazionePolimorfismo
                 this.Abi=abi;
                 this.Cab=cab;
             }
-            public Banca(string id,string denominazione,Utente utente):this(id,denominazione,utente,"06098","14400"){}
+            public Banca(string id,string denominazione,Utente utente):
+            this(id,denominazione,utente,"06098","14400"){}
 
             internal void Preleva(decimal importo)
             {
                 decimal saldo = Utente.Saldo;
                 if(importo>saldo)
                 {
-                    Console.WriteLine($"Scoperto: Operazione di prelievo non ammessa, importo richiesto {importo}");
+                    Console.WriteLine($"Operazione di prelievo non ammessa, importo richiesto {importo}");
                     return;
                     
                 }
